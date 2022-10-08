09cd617cf6c5a8e1f11f9da98104a611b8bf5df4
Айси <IceIX@MacBook-Pro-Filipp.local>
2022-10-08 13:45:41 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.WriteLine("Введите число: ");
+string num3 = Console.ReadLine();
+
+if (num3.Length < 3)
+
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+Index index2 = 2;
+Console.WriteLine(num3[index2]);
+
